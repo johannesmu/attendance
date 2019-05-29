97ff02eae70af7fe9fce63dc36d0533151d831b1
johannes <jmuljana@gmail.com>
2019-05-30 09:12:41 +1000

    added userprofile class

--- src/app/models/userprofile.model.ts
+++ src/app/models/userprofile.model.ts
@@ -1,5 +1,15 @@
-export interface UserProfile{
-  name: string,
-  email: string,
-  picture: string
+export class UserProfile{
+  name: string;
+  email: string;
+  picture: string;
+  constructor(){
+    this.name = '';
+    this.email = '';
+    this.picture = '';
+  }
+  set( name, email, picture ){
+    this.name = name;
+    this.email = email;
+    this.picture = picture;
+  }
 }
--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -5,7 +5,7 @@ import { AngularFireStorage } from '@angular/fire/storage';
 
 
 import { UserProfile } from '../models/userprofile.model';
-import { Camera, CameraOptions } from '@ionic-native/camera';
+
 
 
 @Component({
@@ -15,37 +15,20 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 })
 
 export class ProfilePage implements OnInit {
-  profile: UserProfile;
-  options: CameraOptions = {
-    quality: 50,
-    destinationType: this.camera.DestinationType.FILE_URI,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
-  }
-  currentImage:any;
+  profile: UserProfile = new UserProfile();
+  loading: boolean = true;
   constructor(
-    private camera:Camera,
     private authService:AuthService,
     private storage:AngularFireStorage
 
   ) { }
 
   ngOnInit() {
-    this.authService.authState.subscribe(( data ) => {
+    this.authService.authState.subscribe(( data: any ) => {
       if( data ){
-       this.profile = {name: '', email: data.email, picture: data.photoURL };
-      }
-      else{
-        this.profile = {name:'', email:'', picture:''};
+       this.profile.set(data.displayName,data.email,data.photoURL);
       }
     });
   }
-  getCameraImage(){
-    this.camera.getPicture(this.options).then((imageData) => {
-      this.currentImage = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-     // Handle error
-     console.log("Camera issue:" + err);
-    });
-  }
+  
 }
