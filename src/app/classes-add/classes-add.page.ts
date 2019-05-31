ee72907283bb43364aabf87df955aed2a2b36967
johannes <jmuljana@gmail.com>
2019-06-01 09:42:35 +1000

    add class form added as modal

--- src/app/classes-add/classes-add.page.ts
+++ src/app/classes-add/classes-add.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Class } from '../models/class.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-classes-add',
@@ -7,12 +9,20 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./classes-add.page.scss'],
 })
 export class ClassesAddPage implements OnInit {
-
+  classForm:FormGroup;
+  class:Class;
   constructor(
-    modalController:ModalController
+    private modalController:ModalController,
+    private formBuilder:FormBuilder
   ) { }
 
   ngOnInit() {
+    this.classForm = this.formBuilder.group({
+      name: ['',[ Validators.required, Validators.minLength(3) ]],
+      code:['',[ Validators.required, Validators.minLength(6) ]],
+      startDate: ['', [Validators.required ]],
+      duration: [1,[Validators.required, Validators.min(1) ]]
+    });
   }
   close(){
     this.modalController.dismiss();
